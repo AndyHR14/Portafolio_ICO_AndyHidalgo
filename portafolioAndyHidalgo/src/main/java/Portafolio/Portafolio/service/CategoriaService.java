332a6a13a10bd7f23ebe1c87ef5bfa3fef3a6422
AndyHR14 <andypipoh@gmail.com>
2025-02-19 20:49:28 -0600

    Commit semana 6
    Casi terminado

---
+++ portafolioAndyHidalgo/src/main/java/Portafolio/Portafolio/service/CategoriaService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Portafolio.Portafolio.service;
+
+/**
+ *
+ * @author Andy
+ */
+import Portafolio.Portafolio.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
