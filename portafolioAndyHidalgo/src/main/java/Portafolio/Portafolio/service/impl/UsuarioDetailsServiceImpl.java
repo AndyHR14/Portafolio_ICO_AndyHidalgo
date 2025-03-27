637e207544e193d359d50d2d4875b2769697630a
AndyHR14 <andypipoh@gmail.com>
2025-03-26 18:41:56 -0600

    Sem10
    Todo el codigo de la diapositiva hecho, no funciona correctamente

---
+++ portafolioAndyHidalgo/src/main/java/Portafolio/Portafolio/service/impl/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Portafolio.Portafolio.service.impl;
+
+import Portafolio.Portafolio.dao.UsuarioDao;
+import Portafolio.Portafolio.domain.Rol;
+import Portafolio.Portafolio.domain.Usuario;
+import Portafolio.Portafolio.service.UsuarioDetailsService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Andy
+ */
+@Service("userDetailsService")
+public class UsuarioDetailsServiceImpl implements UsuarioDetailsService, UserDetailsService {
+    @Autowired
+    private UsuarioDao usuarioDao;
+    @Autowired
+    private HttpSession session;
+    
+    @Override
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioDao.findByUsername(username);
+        
+        if (usuario == null){
+            throw new UsernameNotFoundException(username);
+        }
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        var roles = new ArrayList <GrantedAuthority>();
+        for (Rol rol : usuario.getRoles()){
+            roles.add(new SimpleGrantedAuthority(rol.getNombre()));
+        }
+        return new User(usuario.getUsername(), usuario.getPassword(), roles);
+    }
+}
