0a0d4237053ef627c3ed941eb7d3b2b7ea6a3d36
AndyHR14 <andypipoh@gmail.com>
2025-03-28 01:15:48 -0600

    Commit
    Error raro

--- portafolioAndyHidalgo/src/main/java/Portafolio/Portafolio/ProjectConfig.java
+++ portafolioAndyHidalgo/src/main/java/Portafolio/Portafolio/ProjectConfig.java
@@ -1,28 +1,14 @@
 package Portafolio.Portafolio;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author Andy
- */
-
 import java.util.Locale;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
@@ -31,6 +17,9 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
@@ -108,8 +97,10 @@ public class ProjectConfig implements WebMvcConfigurer {
         return http.build();
     }
 
-/* El siguiente método se utiliza para completar la clase no es 
-    realmente funcional, la próxima semana se reemplaza con usuarios de BD
+    
+    /*
+ El siguiente método se utiliza para completar la clase no es 
+    realmente funcional, la próxima semana se reemplaza con usuarios de BD    
     @Bean
     public UserDetailsService users() {
         UserDetails admin = User.builder()
@@ -132,10 +123,9 @@ public class ProjectConfig implements WebMvcConfigurer {
     
     @Autowired
     private UserDetailsService userDetailsService;
-    
+
     @Autowired
-    public void configurerGlobal (AuthenticationManagerBuilder build) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder build) throws Exception {
         build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
-    
